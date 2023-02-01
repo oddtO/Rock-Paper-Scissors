49307d98a66c843a945a34bc5dc1d4b38e8c8197
oddtO <121516833+oddtO@users.noreply.github.com>
2023-02-01 15:29:40 +0200

    Implement random computer choice

--- game.js
+++ game.js
@@ -2,23 +2,15 @@ const ROCK = "Rock";
 const PAPER = "Paper";
 const SCISSORS = "Scissors";
 
-
-function normalizeInput(playerSelection)
-{
-    return playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase();
-
-}
-
-function concatOutcomeString(ifwon, selectionWinner, selectionLoser)
-{
-    return `${ifwon} ${selectionWinner} beats ${selectionLoser}`;
-}
+const LOOKUP_ROCK_PAPER_SCISSORS = [ROCK, PAPER , SCISSORS];
 
 
 
 
 function playRound(playerSelection, computerSelection)
 {
+
+
     playerSelectionNormalized = normalizeInput(playerSelection);
 
 
@@ -32,23 +24,47 @@ function playRound(playerSelection, computerSelection)
     const WON = 'You won!';
     const LOST = 'You lost';
 
-
-
-    
-        if (playerSelectionNormalized == ROCK && computerSelection == PAPER ||
-            playerSelectionNormalized == PAPER && computerSelection == SCISSORS ||
-            playerSelectionNormalized == SCISSORS && computerSelection == ROCK)
-                outcome = concatOutcomeString(LOST, computerSelection, playerSelectionNormalized);
-        else if (
-            playerSelectionNormalized == ROCK && computerSelection == SCISSORS ||
-            playerSelectionNormalized == PAPER && computerSelection == ROCK ||
-            playerSelectionNormalized == SCISSORS && computerSelection == PAPER)
-                outcome = concatOutcomeString(WON, playerSelectionNormalized, computerSelection);
-        else
-            outcome = 'invalid player input';
+    if (playerSelectionNormalized == ROCK && computerSelection == PAPER ||
+        playerSelectionNormalized == PAPER && computerSelection == SCISSORS ||
+        playerSelectionNormalized == SCISSORS && computerSelection == ROCK)
+            outcome = concatOutcomeString(LOST, computerSelection, playerSelectionNormalized);
+    else if (
+        playerSelectionNormalized == ROCK && computerSelection == SCISSORS ||
+        playerSelectionNormalized == PAPER && computerSelection == ROCK ||
+        playerSelectionNormalized == SCISSORS && computerSelection == PAPER)
+            outcome = concatOutcomeString(WON, playerSelectionNormalized, computerSelection);
+    else
+        outcome = 'invalid player input';
     
 
     return outcome;
+
+
+    function normalizeInput(playerSelection)
+    {
+        return playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase();
+
+    }
+
+    function concatOutcomeString(ifwon, selectionWinner, selectionLoser)
+    {
+        return `${ifwon} ${selectionWinner} beats ${selectionLoser}`;
+    }
+
+}
+
+
+
+
+function getComputerChoice()
+{
+    return LOOKUP_ROCK_PAPER_SCISSORS[getRandNumberInRange(0, LOOKUP_ROCK_PAPER_SCISSORS.length)];
+
+
+    function getRandNumberInRange(min, max)
+    {
+        return Math.floor(Math.random() * (max - min) + min);
+    }
 
 
 }
@@ -64,5 +80,10 @@ console.log(playRound('rOcK', SCISSORS));
 console.log(playRound('PaPER', ROCK));
 
 
-console.log(playRound('PaPER', ROCK));
+console.log(playRound('PaER', ROCK));
 console.log(playRound('Rock', ROCK));
+
+
+for(let i = 0; i < 100; ++i)
+    console.log(getComputerChoice());
+
